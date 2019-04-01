ad739e09235f32a7eb0715699b1844148a53d231
ziadaljuhany <ziadaljuhany@gmail.com>
2019-04-01 14:25:45 +0700

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
 	console.log('Connected to server');
-
-	socket.emit('createMessage', {
-		from: 'Ziad',
-		text: 'Yup, that works for me.'
-	});
 });
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
-	socket.emit('newMessage', {
-		from: 'Thowus',
-		text: 'See you then.',
-		createAt: 123123
-	});
-
 	socket.on('createMessage', (message) => {
 		console.log('createMessage', message);
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createAt: new Date().getTime()
+		});
 	});
 
 	socket.on('disconnect', () => {
